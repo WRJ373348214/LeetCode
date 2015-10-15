acd28192aed3aeac9733fccf7bb3572f333662e7
WRJ <18712756685@163.com>
2015-10-15 13:35:31 +0800

    Add new files

---
+++ Easy/Palindrome Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+/*
+    Do this without extra space...没那么苛刻
+    网上和discuss也没有什么牛逼的解法
+                                            */
+    bool isPalindrome(int x) {
+
+    }
+};
